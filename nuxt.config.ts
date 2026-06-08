1aa687ecdf200251892c4e633fa0f1e04db206a6
artuc2025 <artuc2025@gmail.com>
2026-06-08 22:48:18 +0400

    chore: add pinia, nuxt-svgo modules and livetex runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  compatibilityDate: '2024-11-01',
+  modules: ['@pinia/nuxt', 'nuxt-svgo'],
+  runtimeConfig: {
+    public: {
+      livetex: {
+        loaderSrc: process.env.NUXT_PUBLIC_LIVETEX_LOADER_SRC,
+        contactPointId: process.env.NUXT_PUBLIC_LIVETEX_CONTACT_POINT_ID,
+      },
+    },
+  },
 })
